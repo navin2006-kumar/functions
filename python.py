a2aa6f0b06f35906a8f17469926d439f516bbf22
navinkumar-2024-aib <navin2006raja@gmail.com>
2024-11-23 15:57:35 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+def python():
+    print(2+3)
+python()
+
+def add(num_1,num_2):
+    print(num_1+num_2)
+add(5,5)
+
+def sub(num_1,num_2):
+    return num_1-num_2
+print(sub(10,5))
+
+def mul():
+    return 10-5
+print(mul())
